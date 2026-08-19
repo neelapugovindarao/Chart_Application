d64edac4440843c556132d990cf6d623a5dc4577
neelapugovindarao <neelapugovindarao98@gmail.com>
2026-08-19 13:48:09 +0530

    Update client.py

--- client.py
+++ client.py
@@ -8,3 +8,19 @@ name = input("Enter your name: ")
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
+
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode()
+
+            if message == "NAME":
+                client.send(name.encode())
+            else:
+                print(message)
+        except:
+            print("An error occurred!")
+            client.close()
+            break
+
