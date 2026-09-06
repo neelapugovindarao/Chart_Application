a1830a25b06f2e2bd9784965fb68c68bd60876a0
neelapugovindarao <neelapugovindarao98@gmail.com>
2026-09-06 15:43:38 +0530

    Correct input prompt to 'Enter your Name:'

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ import threading
 HOST = '127.0.0.1'
 PORT = 65432
 
-name = input("Enter your name: ")
+name = input("Enter your Name: ")
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
